caf1cd8f48ec2c5ab4e0eeb1d18011052abb56d2
HuDunYu <1410196583@qq.com>
2021-09-20 01:41:47 +0800

    feat:添加注释

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ keyword = [
 ]
 
 
+# 对文本进行特殊处理
 def edit_text(lines):
+    # 删除注释和头文件
     notes_flag = False
     for line in lines[::-1]:
         lines_index = lines.index(line)
@@ -25,12 +27,15 @@ def edit_text(lines):
             del lines[lines_index]
         elif notes_flag is True:
             del lines[lines_index]
-
-        elif '#include' in line or '#define' in line or '#undef' in line or '#ifdef' in line or '#ifndef' in line or '#endif' in line:
+        # 删除头文件
+        elif '#include' in line or '#define' in line or '#undef' in line \
+                or '#ifdef' in line or '#ifndef' in line or '#endif' in line:
             del lines[lines_index]
 
+    # 删除引号间的内容以及一些不必要的标点符号
     for line in lines:
         lines_index = lines.index(line)
+        # 将字符串转化为列表方便修改
         line_str = list(line)
         # 删除引号间的内容
         if '"' in line:
@@ -42,30 +47,36 @@ def edit_text(lines):
             str_index = line_str.index(unit)
             if unit == '(' or unit == ')' or unit == '[' or unit == ']' or unit == ':' or unit == ';':
                 line_str[str_index] = ' '
+        # 将列表转化为字符串
         line = ''.join(line_str)
+        # 更新文本
         lines[lines_index] = line
 
     # 如果遇到 ‘{’，让其前后增加空格
     for line in lines:
         lines_index = lines.index(line)
+        # 将字符串转化为列表方便修改
         line_list = list(line)
         index = line.find('{')
         if index >= 0:
             line_list.insert(index + 1, ' ')
             line_list.insert(index, ' ')
+            # 将列表转化为字符串
             line = ''.join(line_list)
+            # 更新文本
             lines[lines_index] = line
 
     # 将每行结尾换行前加上空格
     for line in lines:
         lines_index = lines.index(line)
+        # 将字符串转化为列表方便修改
         line_list = list(line)
         line_list[len(line_list) - 1] = ' '
         line_list.append('\n')
+        # 将列表转化为字符串
         line = ''.join(line_list)
+        # 更新文本
         lines[lines_index] = line
-    for line in lines:
-        print(line)
     return lines
 
 
@@ -84,9 +95,9 @@ def count_keyword(lines):
 
 # 计算 switch 和 case 个数
 def count_switch(lines):
-    flag = False
-    temp_num = 0
-    stack_num = 0
+    flag = False    # 判断当前是否处在一个 switch case 结构中
+    temp_num = 0    # 每次一组 switch case 中 case 的个数
+    stack_num = 0   # 栈中元素个数
     switch_num = 0
     case_num = []
     for line in lines:
@@ -112,13 +123,14 @@ def count_switch(lines):
 def count_if_else(lines):
     if_else_num = 0
     if_elseif_else_num = 0
-    if_else_stack = []
-    if_flag = False
-    else_if_flag = False
-    else_flag = False
+    if_else_stack = []  # 用栈完成匹配
+    if_flag = False     # 判断是否在一个 if 中
+    else_if_flag = False    # 是否有 else if
+    else_flag = False       # 是否有 else
     for line in lines:
         lines_index = lines.index(line)
         str = line.split(' ')
+        # 当遇到 if，else if，else，{ 直接入栈
         if 'else' in str and 'if' in str:
             if_else_stack.append('else if')
         elif 'if' in str:
@@ -128,18 +140,22 @@ def count_if_else(lines):
             if_else_stack.append('else')
         if '{' in str and if_flag is True:
             if_else_stack.append('{')
+        # 当遇到 } 需要出栈，直到匹配到 {
         if '}' in str and if_flag is True:
             temp = if_else_stack.pop()
             if temp == '{':
                 continue
             while True:
                 if temp == 'else':
+                    # 存在else
                     else_flag = True
                     temp = if_else_stack.pop()
                 elif temp == 'else if':
+                    # 存在 else if
                     else_if_flag = True
                     temp = if_else_stack.pop()
                 elif temp == 'if':
+                    # 遇到 if，判断是else if结构还是
                     if else_flag is True and else_if_flag is True:
                         if_elseif_else_num += 1
                     elif else_flag is True and else_if_flag is False:
@@ -149,6 +165,7 @@ def count_if_else(lines):
                     else_if_flag = False
                     else_flag = False
                     break
+    # 栈中还会存在 if，else if,else，需要全部出栈匹配
     while len(if_else_stack) > 0:
         temp = if_else_stack.pop()
         if temp == 'else':
@@ -162,12 +179,15 @@ def count_if_else(lines):
     return if_else_num, if_elseif_else_num
 
 
+# 输入
 file_path = input()
 rank = int(input())
+
+# 读取文件
 with open(file_path) as file_object:
     lines = file_object.readlines()
-    text = file_object.read()
 
+# 对文件进行必要处理
 lines = edit_text(lines)
 
 if rank >= 1:
